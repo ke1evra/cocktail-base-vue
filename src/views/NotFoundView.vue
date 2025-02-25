d54a05c6f94e97ab04c7a6a68e480406dd87262f
Konstantin Dolgov <dolgov.konstantin@gmail.com>
2025-02-25 09:15:29 +0300

    some 404 fun

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="flex bg-stone-400 justify-between h-screen">
     <div class="flex flex-grow max-w-[1024px] min-w-[360px] mx-auto bg-stone-200 shadow-2xl">
-      <div class="flex flex-col gap-3 p-3">
-        <div class="text-3xl">404</div>
-        <RouterLink :to="{ name: 'home' }" class="bg-stone-300 p-3">Home</RouterLink>
+      <div class="flex flex-col p-3 w-full items-center justify-center gap-3">
+        <div class="text-7xl">404</div>
+
+        <div>
+          <img src="/404.webp" class="h-48" alt="I have no idea" />
+        </div>
+
+        <RouterLink :to="{ name: 'home' }" class="bg-stone-300 p-5">Go home</RouterLink>
       </div>
     </div>
   </div>
